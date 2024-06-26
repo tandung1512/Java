d33a7d06ce82d23bb11d0af26df6e8b033f85c7f
tandung1512 <tandung15122003@gmail.com>
2024-06-26 14:52:51 +0700

    Fisrt comit

---
+++ src/main/java/com/asm/Pk02356NguyenTanDungJavaApplication.java
@@ -0,0 +1,13 @@
+package com.asm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pk02356NguyenTanDungJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pk02356NguyenTanDungJavaApplication.class, args);
+	}
+
+}
